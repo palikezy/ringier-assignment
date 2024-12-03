18605c22131a3bf695dd891a23b27f9ad946a06b
Pavol Ondrejka <palo.ondrejka@gmail.com>
2024-12-03 09:47:38 +0100

    feat: change default model

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ollama_url: str | None = None
-    llm_model: str = "mistral"
+    llm_model: str = "llama3.2:1b"
     forecast_api_key: str = ":)"
     forecast_detail: bool = True
     forecast_metric: bool = True
